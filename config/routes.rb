682b3730fed62fb969e50d3e617099245e627b10
Alexey Park <alexey.g.park@gmail.com>
2017-04-06 02:05:39 +0600

    Locales added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
     root 'products#index'
 
-    devise_for :users
-
     namespace :admin do
       root 'products#index'
       resources :products
@@ -12,6 +10,7 @@ Rails.application.routes.draw do
     end
 
   end
+  devise_for :users
   match "*path", to: redirect("/#{I18n.default_locale}/%{path}"), via: :all
   match "", to: redirect("/#{I18n.default_locale}/"), via: :all
 end
